8d8bb1bbaa57914f5e5b3f645df7d312918a4f30
Daniel Desira <dannydes@users.noreply.github.com>
2014-11-22 20:35:30 +0100

    Corrected spacing

--- rss.js
+++ rss.js
@@ -2,35 +2,35 @@
 
   'use strict';
 
-  function fetchAttr( item, tagName ) {
-    return item.getElementsByTagName( tagName )[0].textContent;
+  function fetchAttr(item, tagName) {
+    return item.getElementsByTagName(tagName)[0].textContent;
   }
 
-  function Feed( source ) {
-    if ( typeof source === 'string' ) {
-      this.parse( source );
-    } else if ( source ) {
+  function Feed(source) {
+    if (typeof source === 'string') {
+      this.parse(source);
+    } else if (source) {
       this.items = source.items;
     } else {
       this.items = [];
     }
   }
   
-  function FeedItem( item ) {
-    this.title = fetchAttr( item, 'title' );
-    this.link = fetchAttr( item, 'link' );
-    this.description = fetchAttr( item, 'description' );
+  function FeedItem(item) {
+    this.title = fetchAttr(item, 'title');
+    this.link = fetchAttr(item, 'link');
+    this.description = fetchAttr(item, 'description');
   }
 
-  function parse( xml ) {
+  function parse(xml) {
     var count, item,
       parser = new DOMParser(),
-      DOMitems = parser.parseFromString( xml, 'application/xml' ).getElementsByTagName( 'item' );
+      DOMitems = parser.parseFromString(xml, 'application/xml').getElementsByTagName('item');
 
     this.items = [];
     
-    for ( count = 0; count < DOMitems.length; count++ ) {
-      this.items[count] = new FeedItem( DOMitems[count] );
+    for (count = 0; count < DOMitems.length; count++) {
+      this.items[count] = new FeedItem(DOMitems[count]);
     }
 
   }
